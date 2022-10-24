0b1bba4343f4afcb30ae9f3e7fe83f35beec25b2
1349636970 <a1349636970@gmail.com>
2022-10-24 02:08:20 -0700

    Login page - add background image; add login form

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ if(process.env.NODE_ENV === 'development') {
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var loginRouter = require('./routes/login')
 
 
 
@@ -27,10 +27,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/login', loginRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
