ac52fc314c6acfb22f39833c33615c91a8e620aa
a1349636970 <a1349636970@gmail.com>
2022-10-24 09:51:44 -0700

    Start working on lobby

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login')
 var registerRouter = require('./routes/register')
+var lobbyRouter = require('./routes/lobby')
 
 
 
@@ -35,6 +36,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter)
 app.use('/register', registerRouter)
+app.use('/lobby', lobbyRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
