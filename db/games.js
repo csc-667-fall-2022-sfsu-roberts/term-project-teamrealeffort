b8685efb69ff42e533b1fd27ccab7eb66bf5de42
Zhenyu Lin <a1349636970@gmail.com>
2022-12-07 14:07:16 -0800

    lobby chat finish styling

--- db/games.js
+++ db/games.js
@@ -3,7 +3,7 @@ const db = require("./index");
 const CREATE_SQL = "INSERT INTO games (title) VALUES(${title}) RETURNING id";
 const ADD_USER_SQL = "INSERT INTO game_users (user_id, game_id) VALUES(${user_id}, ${game_id}) RETURNING game_id";
 const CHECK_USER_IN_GAME_SQL = "SELECT * FROM game_users WHERE user_id =${user_id}";
-const LIST_SQL = "SELECT * FROM games"
+const LIST_SQL = "select * from games"
 
 const create = (user_id, title) => {
   console.log("Title: " + title);
@@ -31,6 +31,6 @@ const addUser = (user_id, game_id) => {
 const all = () => {
   return db.any(LIST_SQL).then((games) => {
     return games;
-  });
+  }).catch((e)=>{console.log(e)});
 };
 module.exports = { create, addUser, all };
