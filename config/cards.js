951fbb6221c465b8b7be3349259d72402a9db910
Zhenyu Lin <a1349636970@gmail.com>
2022-12-11 13:24:30 -0800

    Revert "merge frontend and backend"

--- config/cards.js
+++ config/cards.js
@@ -1,44 +1,18 @@
-// 10: Draw Two
-// 11: Draw Four
-// 12: Wild
-// 13: Reverse
-// 14: Skip
 const DRAW_TWO = 10;
-const DRAW_FOUR = 11;
+const WILD_DRAW_FOUR = 11;
 const WILD = 12;
 const REVERSE = 13;
 const SKIP = 14;
 
-const SINGLE_COLOR_CARD_TYPES = [0];
-
-const DUAL_COLOR_CARD_TYPES = [
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  DRAW_TWO,
-  REVERSE,
-  SKIP,
-];
-
-const NO_COLOR_CARD_TYPES = [DRAW_FOUR, WILD];
-
 const BLUE = "blue";
+const GREEN = "green";
 const YELLOW = "yellow";
 const RED = "red";
-const GREEN = "green";
 const NONE = "none";
 
-const CARD_COLORS = { BLUE, YELLOW, RED, GREEN, NONE };
+const CARDS_WITH_COLORS_SINGLE = [0];
+const CARDS_WITH_COLORS_MULTIPLE = [1, 2, 3, 4, 5, 6, 7, 8, 9, DRAW_TWO, REVERSE, SKIP];
+const OTHER_CARDS = [WILD_DRAW_FOUR, WILD];
+const CARD_COLORS = { BLUE, GREEN, YELLOW, RED, NONE };
 
-module.exports = {
-  SINGLE_COLOR_CARD_TYPES,
-  DUAL_COLOR_CARD_TYPES,
-  NO_COLOR_CARD_TYPES,
-  CARD_COLORS,
-};
+module.exports = { CARD_COLORS, CARDS_WITH_COLORS_SINGLE, CARDS_WITH_COLORS_MULTIPLE, OTHER_CARDS };
